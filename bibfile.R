537424b76e15858e0b4dee05eb52198fda37d04e
Rob J Hyndman <robjhyndman@gmail.com>
2018-08-09 12:38:53 +1000

    Removed all abstracts and keywords

--- bibfile.R
+++ bibfile.R
@@ -1,4 +1,9 @@
 library(RefManageR)
 
 rjh <- ReadBib("rjhpubs.bib", check="warn")
+for(i in seq_along(rjh))
+{
+  rjh[[i]]$abstract <- NULL
+  rjh[[i]]$keywords <- NULL
+}
 WriteBib(sort(rjh, sorting="ydnt"), "rjhpubs.bib")
