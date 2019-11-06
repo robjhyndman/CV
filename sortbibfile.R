5240dfb4d6ef565588f2f77cecf627fe4920c39c
robjhyndman <robjhyndman@gmail.com>
2019-11-06 13:39:06 +1100

    Updated packages and papers

--- sortbibfile.R
+++ sortbibfile.R
@@ -1,11 +1,11 @@
-#devtools::install_github("ropensci/RefManageR")
+# devtools::install_github("ropensci/RefManageR")
 library(RefManageR)
 
-rjh <- ReadBib("rjhpubs.bib", check="warn")
-for(i in seq_along(rjh))
-{
+rjh <- ReadBib("rjhpubs.bib", check = "warn")
+for (i in seq_along(rjh)) {
   rjh[[i]]$abstract <- NULL
   rjh[[i]]$keywords <- NULL
 }
-WriteBib(sort(rjh, sorting="ydnt"), "rjhpubs.bib")
+WriteBib(sort(rjh, sorting = "ydnt"), "rjhpubs.bib")
 # Beware: Carta2011 authors get screwed up by RefManageR
+# Replace with author = {Davide Carta and Laura Villanova and Stefano Costacurta and Alessandro Patelli and Irene Poli and Simone Vezzù and Paolo Scopece and Fabio Lisi and Kate Smith-Miles and Rob J Hyndman and Anita J Hill and Paolo Falcaro},
