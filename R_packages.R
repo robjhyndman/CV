ce1837e29337bfc5fe98f17b98426bf0fed542de
Rob J Hyndman <robjhyndman@gmail.com>
2022-01-09 13:46:20 +1100

    Put github repos in a file

--- R_packages.R
+++ R_packages.R
@@ -3,53 +3,6 @@ library(pkgmeta)
 library(RefManageR)
 source("functions.R")
 
-github <- c(
-  "AndriSignorell/DescTools",
-  "AU-BURGr/ozdata",
-  "earowang/hts",
-  "earowang/sugrrants",
-  "eddelbuettel/binb",
-  "FinYang/tsdl",
-  "haghbinh/sfar",
-  "jforbes14/eechidna",
-  "mitchelloharawild/fasster",
-  "mitchelloharawild/vitae",
-  "numbats/monash",
-  "pridiltal/oddstream",
-  "pridiltal/oddwater",
-  "pridiltal/stray",
-  "robjhyndman/addb",
-  "robjhyndman/anomalous",
-  "robjhyndman/compenginets",
-  "robjhyndman/cricketdata",
-  "robjhyndman/demography",
-  "robjhyndman/expsmooth",
-  "robjhyndman/fma",
-  "robjhyndman/forecast",
-  "robjhyndman/fpp2-package",
-  "robjhyndman/fpp3-package",
-  "robjhyndman/hdrcde",
-  "robjhyndman/Mcomp",
-  "robjhyndman/MEFM-package",
-  "robjhyndman/pkgmeta",
-  "robjhyndman/thief",
-  "robjhyndman/tscompdata",
-  "robjhyndman/tsfeatures",
-  "ropenscilabs/ozbabynames",
-  "ropenscilabs/rcademy",
-  "sayani07/gravitas",
-  "sevvandi/lookout",
-  "thiyangt/seer",
-  "tidyverts/fable",
-  "tidyverts/fabletools",
-  "tidyverts/feasts",
-  "tidyverts/tsibble",
-  "tidyverts/tsibbledata",
-  "verbe039/bfast",
-  "ykang/gratis",
-  NULL
-)
-
 # Check if this has been run in last day
 recent_run <- fs::file_exists(here::here("packages.rds"))
 if (recent_run) {
@@ -63,7 +16,7 @@ if (recent_run) {
   rjh_packages <- pkgmeta::get_meta(
     cran_author = "Hyndman",
     include_downloads=TRUE, start="2015-01-01",
-    github_repos = github
+    github_repos = read.table("github_r_repos.txt")$V1
   )
   # Save result and return it
   saveRDS(rjh_packages, file = here::here("packages.rds"))
