be9a33b11016e79d72aa4815695bd8dd4c32d887
Rob Hyndman <robjhyndman@gmail.com>
2023-04-10 14:49:19 +1000

    Improved Google scholar code

--- gscholar.R
+++ gscholar.R
@@ -5,22 +5,20 @@ if(file.exists("gspapers.rds")) {
   gspapers <- readRDS("gspapers.rds")
 } else {
   # Grab all citation info, 100 per call
-  gspapers1 <- gcite_url(
-    url = "https://scholar.google.com.au/citations?user=vamErfkAAAAJ&hl=en",
-    cstart = 0, pagesize = 100
-  ) %>%
+  gspapers <- list()
+  complete <- FALSE
+  while(!complete) {
+    k <- length(gspapers)
+    gspapers[[k+1]] <- gcite_url(
+      url = "https://scholar.google.com.au/citations?user=vamErfkAAAAJ&hl=en",
+      cstart = k*100,
+      pagesize = 100
+    ) |>
     gcite_papers()
-  gspapers2 <- gcite_url(
-    url = "https://scholar.google.com.au/citations?user=vamErfkAAAAJ&hl=en",
-    cstart = 100, pagesize = 100
-  ) %>%
-    gcite_papers()
-  gspapers3 <- gcite_url(
-    url = "https://scholar.google.com.au/citations?user=vamErfkAAAAJ&hl=en",
-    cstart = 200, pagesize = 100
-  ) %>%
-    gcite_papers()
-  gspapers <- bind_rows(gspapers1, gspapers2, gspapers3) %>%
+    if(NROW(gspapers[[k+1]]) < 100)
+      complete <- TRUE
+  }
+  gspapers <- bind_rows(gspapers) |>
     as_tibble()
   saveRDS(gspapers, "gspapers.rds")
 }
