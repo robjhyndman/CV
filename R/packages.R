8df7b9026c1b75bae9e7ed48c3a9485ddc14f120
Rob J Hyndman <robjhyndman@gmail.com>
2026-09-24 12:28:01 +1000

    Don't include icons package

--- R/packages.R
+++ R/packages.R
@@ -51,9 +51,9 @@ get_rjh_packages <- function(rjh_packages) {
         url
       ),
     )
-  # Remove fpp and rmarkdown
+  # Remove fpp, rmarkdown and icons (icons is by a different Hyndman)
   rjh_packages <- rjh_packages |>
-    filter(package %notin% c("fpp", "rmarkdown"))
+    filter(package %notin% c("fpp", "rmarkdown", "icons"))
 
   # Return tibble of package info
   return(rjh_packages)
